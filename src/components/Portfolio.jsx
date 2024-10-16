ac56daaa5cb9d491b9cacc67868d0c4f9f387f2c
Steve Mogan Odumbe <124534650+StevetheRebel@users.noreply.github.com>
2024-10-16 20:19:53 +0300

    edits

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Portfolio() {
+  return (
+    <>
+    <div className='h-dvh pt-16'>
+        Portfolio
+    </div>
+    </>
+  )
+}
+
+export default Portfolio
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 import Footer from "../Footer";
 import Logo from "./../../assets/Logo.png";
+import Client from "../Client";
+import Portfolio from "../Portfolio";
 
 function Home() {
+  const portfolioRef = useRef(null);
+
+  const scrollToPortfolio = () => {
+    portfolioRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <>
       <div className="h-auto relative">
-        <section className="mt-16 pb-48 w-full flex flex-col items-center justify-center font-mono sm:flex-row md:pb-20 ">
+        <section className="mt-16 pb-8 w-full flex flex-col items-center justify-center font-mono sm:flex-row relative ">
           <div className="flex flex-col gap-4 py-12 px-2 sm:w-1/2 sm:my-0 sm:px-8 ">
             <h1 className="text-3xl text-balance font-bold sm:text-5xl w-full">
               Freelance UI Designer & Web Developer
@@ -31,10 +39,14 @@ function Home() {
           <div className="flex items-center justify-center bg-robertimg bg-no-repeat bg-center bg-contain sm:w-1/2">
             <img src={Logo} alt="robert fox" className="opacity-15 sm:p-16" />
           </div>
-          <button className="absolute text-sm bottom-48 right-2 px-4 py-2 z-2 bg-midPurple hover:bg-darkPurple rounded-3xl text-white sm:text-base sm:bottom-24 sm:right-10 ">
+          <button className="absolute text-sm bottom-0 right-2 px-4 py-2 z-2 bg-midPurple hover:bg-darkPurple rounded-3xl text-white sm:text-base sm:bottom-10 sm:right-5 " onClick={scrollToPortfolio}>
             Portfolio
           </button>
         </section>
+        <Client />
+        <div ref={portfolioRef}>
+          <Portfolio />
+        </div>
         <Footer />
       </div>
     </>
